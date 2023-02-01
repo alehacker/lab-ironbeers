0fef61275d73577a1e32f7db1592b1b29b26d11a
Ale Kennedy <alepertuz@gmail.com>
2023-02-01 17:02:47 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/beers', ((req, res, next) =>{
+    //logic for it goes here
+}))
+
 module.exports = router;
