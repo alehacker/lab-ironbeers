415df6c7d87b303a6a08d67f6440c4e21ab9d4af
Ale Kennedy <alepertuz@gmail.com>
2023-02-01 21:17:00 -0500

    Beer and Random Beer views added- Does not work

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,30 @@ const punkAPI = new PunkAPIWrapper()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IronBeers' });
 });
 
 router.get('/beers', ((req, res, next) =>{
-    punkAPI.getBeers().then()
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+        // console.log('Beers from the database: ', beersFromApi)
+        const beerData = {beersFromApi};
+        // console.log("these are the 25 beers", beerData)
+        res.render("beers.hbs", beerData)
+    })
+    .catch(error => console.log(error));
+}))
+
+router.get('/random-beer', ((req, res, next)=>{
+    punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+    const random = { responseFromAPI };
+    res.render('random-beer', random);
+    })
+    .catch(error => console.log(error));
+
 }))
 
 module.exports = router;
